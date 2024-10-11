5ea19ae272093c4a9d57c1aa55ba011420db849d
YazanMahfooz <151470249+YAZAN420@users.noreply.github.com>
2024-10-11 14:23:17 +0300

    Update EspCode.ino

--- EspCode/EspCode.ino
+++ EspCode/EspCode.ino
@@ -5,11 +5,46 @@ const char* ssid = "lolia";
 const char* password = "11111111";
 ESP8266WebServer server(80); 
 int x=0,y=0,dif=40,mode=-1;
-int echoPin=D7;
-int trigPin=D8;
-int duration,distance;
-int speedmove,speedturn;
-int Motor1pin1=D1,Motor1pin2=D2,Motor2pin1=D3,Motor2pin2=D4,SpeedRight=D5,speedLeft=D6;
+int duration,distance,speedmove;
+int Motor1pin1=D1,Motor1pin2=D2,Motor2pin1=D3,Motor2pin2=D4,SpeedRight=D5,speedLeft=D6,echoPin=D7,trigPin=D8;
+void stop(){
+    digitalWrite(Motor1pin1,LOW);
+    digitalWrite(Motor1pin2,LOW);
+    digitalWrite(Motor2pin1,LOW);
+    digitalWrite(Motor2pin2,LOW);
+}
+void moveUp(){
+    analogWrite(SpeedRight,speedmove);
+    analogWrite(speedLeft,speedmove);
+    digitalWrite(Motor1pin1,LOW);
+    digitalWrite(Motor1pin2,HIGH);
+    digitalWrite(Motor2pin1,LOW);
+    digitalWrite(Motor2pin2,HIGH);
+}
+void moveDown(){
+    analogWrite(SpeedRight,speedmove);
+    analogWrite(speedLeft,speedmove);
+    digitalWrite(Motor1pin1,HIGH);
+    digitalWrite(Motor1pin2,LOW);
+    digitalWrite(Motor2pin1,HIGH);
+    digitalWrite(Motor2pin2,LOW);
+}
+void moveRight(){
+    analogWrite(SpeedRight,speedmove);
+    analogWrite(speedLeft,speedmove);
+    digitalWrite(Motor1pin1,LOW);
+    digitalWrite(Motor1pin2,HIGH);
+    digitalWrite(Motor2pin1,HIGH);
+    digitalWrite(Motor2pin2,LOW);
+}
+void moveLeft(){
+    analogWrite(SpeedRight,speedmove);
+    analogWrite(speedLeft,speedmove);
+    digitalWrite(Motor1pin1,HIGH);
+    digitalWrite(Motor1pin2,LOW);
+    digitalWrite(Motor2pin1,LOW);
+    digitalWrite(Motor2pin2,HIGH);
+}
 void setup() {
   pinMode(Motor1pin1,OUTPUT);
   pinMode(Motor1pin2,OUTPUT);
@@ -53,64 +88,34 @@ void setup() {
 }
 void loop() {
     server.handleClient();
-    digitalWrite(trigPin,LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin,LOW);
-    duration=pulseIn(echoPin,HIGH);
-    distance=duration*0.034/2;
     if(mode==1){
-      if(y>10){
-            speedmove=map(y,0,180,0,200);
-            analogWrite(SpeedRight,speedmove);
-            analogWrite(speedLeft,speedmove);
-            digitalWrite(Motor1pin1,HIGH);
-            digitalWrite(Motor1pin2,LOW);
-            digitalWrite(Motor2pin1,HIGH);
-            digitalWrite(Motor2pin2,LOW);
-      }else if(x<dif&&x>-1*dif&&y>-1*dif&&y<dif){
-            digitalWrite(Motor1pin1,LOW);
-            digitalWrite(Motor1pin2,LOW);
-            digitalWrite(Motor2pin1,LOW);
-            digitalWrite(Motor2pin2,LOW);
-            x=0;y=0;
-      }else if(x>=dif){
-            speedmove=map(x,0,180,0,200);
-            analogWrite(SpeedRight,speedmove);
-            analogWrite(speedLeft,speedmove);
-            digitalWrite(Motor1pin1,HIGH);
-            digitalWrite(Motor1pin2,LOW);
-            digitalWrite(Motor2pin1,LOW);
-            digitalWrite(Motor2pin2,HIGH);
-      }else if(x<=-1*dif){
-            speedmove=map(x,0,-65,0,200);
-            analogWrite(SpeedRight,speedmove);
-            analogWrite(speedLeft,speedmove);
-            digitalWrite(Motor1pin1,LOW);
-            digitalWrite(Motor1pin2,HIGH);
-            digitalWrite(Motor2pin1,HIGH);
-            digitalWrite(Motor2pin2,LOW);
-      }else if(y<-1*dif){
-            speedmove=map(y,0,-90,0,200);
-            analogWrite(SpeedRight,speedmove);
-            analogWrite(speedLeft,speedmove);
-            digitalWrite(Motor1pin1,LOW);
-            digitalWrite(Motor1pin2,HIGH);
-            digitalWrite(Motor2pin1,LOW);
-            digitalWrite(Motor2pin2,HIGH);
-      }
+            if(y>10){
+                  speedmove=map(y,0,180,0,200);
+                  moveDown();
+            }else if(x<dif&&x>-1*dif&&y>-1*dif&&y<dif){
+                  stop();
+            }else if(x>=dif){
+                  speedmove=map(x,0,180,0,200);
+                  moveLeft();
+            }else if(x<=-1*dif){
+                  speedmove=map(x,0,-65,0,200);
+                  moveRight();
+            }else if(y<-1*dif){
+                  speedmove=map(y,0,-90,0,200);
+                  moveUp();
+            }
     }else if(mode==2){
+            digitalWrite(trigPin,LOW);
+            delayMicroseconds(2);
+            digitalWrite(trigPin,HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigPin,LOW);
+            duration=pulseIn(echoPin,HIGH);
+            distance=duration*0.034/2;
             if(distance>40){
-                    digitalWrite(Motor1pin1,LOW);
-                    digitalWrite(Motor1pin2,HIGH);
-                    digitalWrite(Motor2pin1,LOW);
-                    digitalWrite(Motor2pin2,HIGH);
+                moveUp();
             }else{
-                    digitalWrite(Motor1pin1,LOW);
-                    digitalWrite(Motor1pin2,HIGH);
-                    digitalWrite(Motor2pin1,HIGH);
-                    digitalWrite(Motor2pin2,LOW);
+                moveRight();
             }
             Serial.println(distance);
     }
